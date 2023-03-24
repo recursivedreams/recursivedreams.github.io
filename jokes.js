4c29c673f0b533a4d6b7fe1267db708f39099757
recursivedreams <93991535+recursivedreams@users.noreply.github.com>
2023-03-24 10:54:12 +0100

    Add files via upload

--- jokes.js
+++ jokes.js
@@ -3,16 +3,69 @@
   <head>
     <meta charset="UTF-8">
     <title>OpenAI Button Example</title>
+    <style>
+      body {
+        font-family: sans-serif;
+        margin: 0;
+        padding: 0;
+      }
+      
+      #container {
+        margin: 50px auto;
+        max-width: 800px;
+        text-align: center;
+      }
+      
+      h1 {
+        margin-bottom: 20px;
+      }
+      
+      label {
+        display: block;
+        font-weight: bold;
+        margin-bottom: 10px;
+        text-align: left;
+      }
+      
+      input[type="text"] {
+        font-size: 16px;
+        padding: 10px;
+        width: 100%;
+      }
+      
+      button {
+        background-color: #4CAF50;
+        border: none;
+        color: white;
+        cursor: pointer;
+        font-size: 16px;
+        margin-top: 20px;
+        padding: 10px 20px;
+        transition: background-color 0.3s;
+      }
+      
+      button:hover {
+        background-color: #3e8e41;
+      }
+      
+      #response {
+        background-color: #f2f2f2;
+        border: 1px solid #ddd;
+        margin-top: 20px;
+        padding: 20px;
+        text-align: left;
+        white-space: pre-wrap;
+      }
+    </style>
   </head>
   <body>
-    <h1>OpenAI Button Example</h1>
-    <label for="prompt">Prompt:</label>
-    <input type="text" id="prompt" name="prompt" value="What is the meaning of life?">
-    <br>
-    <button onclick="generateResponse()">Generate Response</button>
-    <br>
-    <br>
-    <p id="response"></p>
+    <div id="container">
+      <h1>OpenAI Button Example</h1>
+      <label for="prompt">Prompt:</label>
+      <input type="text" id="prompt" name="prompt" value="What is the meaning of life?">
+      <button onclick="generateResponse()">Generate Response</button>
+      <div id="response"></div>
+    </div>
     
     <script src="https://cdn.openai.com/sdk/js/0.5.0/openai.min.js"></script>
     <script>
@@ -33,7 +86,9 @@
           temperature: 0.7,
         }).then(response => {
           // Set the response text in the HTML page
-          document.getElementById('response').innerHTML = response.choices[0].text;
+          const responseDiv = document.getElementById('response');
+          responseDiv.innerHTML = '<strong>Response:</strong><br>' + response.choices[0].text;
+          responseDiv.style.display = 'block';
         }).catch(error => {
           // Handle any errors
           console.log(error);
